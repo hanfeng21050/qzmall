b2e5d705788425507136525892aff09c940c6f4d
hanfeng <hanfeng21050@163.com>
2020-12-02 10:07:54 +0800

    基本完成产品详情页的前端页面展示

--- src/views/product-detail.vue
+++ src/views/product-detail.vue
@@ -145,6 +145,7 @@
       </div>
     </div>
     <div class="w">
+      <!-- 侧边栏 -->
       <div class="aside">
         <div class="mt">
           <h4>选购达人</h4>
@@ -182,68 +183,221 @@
           </ul>
         </div>
       </div>
-
-      <div class="detail">
-        <div class="tab-main">
-          <ul>
-            <li>商品介绍</li>
-            <li>规格与包装</li>
-            <li>售后保障</li>
-            <li>商品评价(4万+)</li>
-          </ul>
-        </div>
-
-        <div class="tab-con">
-          <ul>
-            <li>
-              <div class="jieshao">
-                <p>
-                  <a href="#">品牌:华为（HUAWEI）</a>
-                </p>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <a href="##">商品名称：华为Mate 10</a>
-                      </td>
-                      <td>
-                        <a href="##">商品毛重：0.58kg</a>
-                      </td>
-                      <td>
-                        <a href="##">商品编号：5544038</a>
-                      </td>
-                      <td>
-                        <a href="##">商品产地：中国大陆</a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <a href="##">系统：安卓（Android）</a>
-                      </td>
-                      <td>
-                        <a href="##">前置摄像头像素：800万-1599万</a>
-                      </td>
-                      <td>
-                        <a href="##">后置摄像头像素：2000万及以上，1200万-1999万</a>
-                      </td>
-                      <td>
-                        <a href="##">机身内存：128GB</a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colspan="4">
-                        <a href="##">全面屏，双卡双待，指纹识别，Type-C，VoLTE，2K屏，拍照神器，支持NFC，商务手机，安全手机，分辨率10</a>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <img class="xiaoguo" src="../style/img/介绍.jpg">
+      <!-- 商品详情 -->
+      <el-tabs type="border-card" class="detail">
+        <el-tab-pane label="商品介绍">
+          <div class="tab-con">
+            <ul>
+              <li>
+                <div class="jieshao">
+                  <p>
+                    <a href="#">品牌:华为（HUAWEI）</a>
+                  </p>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <a href="##">商品名称：华为Mate 10</a>
+                        </td>
+                        <td>
+                          <a href="##">商品毛重：0.58kg</a>
+                        </td>
+                        <td>
+                          <a href="##">商品编号：5544038</a>
+                        </td>
+                        <td>
+                          <a href="##">商品产地：中国大陆</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <a href="##">系统：安卓（Android）</a>
+                        </td>
+                        <td>
+                          <a href="##">前置摄像头像素：800万-1599万</a>
+                        </td>
+                        <td>
+                          <a href="##">后置摄像头像素：2000万及以上，1200万-1999万</a>
+                        </td>
+                        <td>
+                          <a href="##">机身内存：128GB</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colspan="4">
+                          <a href="##">全面屏，双卡双待，指纹识别，Type-C，VoLTE，2K屏，拍照神器，支持NFC，商务手机，安全手机，分辨率10</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <img class="xiaoguo" src="../style/img/介绍.jpg">
+                </div>
+              </li>
+            </ul>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="规格与包装">
+          <div class="guiGebox">
+            <div class="guiGe">
+              <h3>主体</h3>
+              <dl>
+                <dt>品牌</dt>
+                <dd>华为(HUAWEI)</dd>
+                <dt>型号</dt>
+                <dd>ALP-AL00</dd>
+                <dt>入网型号</dt>
+                <dd>ALP-AL00</dd>
+                <dt>上市年份</dt>
+                <dd>2017年</dd>
+                <dt>上市时间</dt>
+                <dd>10月</dd>
+              </dl>
+            </div>
+            <div class="guiGe">
+              <h3>基本信息</h3>
+              <dl>
+                <dt>机身颜色</dt>
+                <dd>亮黑色</dd>
+                <dt>机身长度（mm）</dt>
+                <dd>150.5</dd>
+                <dt>机身宽度（mm）</dt>
+                <dd>77.8</dd>
+                <dt>机身厚度（mm）</dt>
+                <dd>8.2</dd>
+                <dt>机身重量（g）</dt>
+                <dd>约186g（含电池）</dd>
+                <dt>输入方式</dt>
+                <dd>触控</dd>
+                <dt>运营商标志与内容</dt>
+                <dd>无</dd>
+                <dt>机身材质分类</dt>
+                <dd>金属边框；玻璃后盖</dd>
+                <dt>机身材质工艺</dt>
+                <dd>四曲面玻璃后壳</dd>
+              </dl>
+            </div>
+            <div class="guiGe">
+              <h3>操作系统</h3>
+              <dl>
+                <dt>操作系统</dt>
+                <dd>Android</dd>
+                <dt>操作系统版本</dt>
+                <dd>华为 EMUI 8.0</dd>
+              </dl>
+            </div>
+            <div class="guiGe">
+              <h3>主芯片</h3>
+              <dl>
+                <dt>CPU品牌</dt>
+                <dd>海思（Hisilicon）</dd>
+                <dt>CPU频率</dt>
+                <dd>4*Cortex A73 2.36GHz + 4*Cortex A53 1.8GHz</dd>
+                <dt>CPU核数</dt>
+                <dd>八核</dd>
+                <dt>CPU型号</dt>
+                <dd>麒麟970</dd>
+              </dl>
+            </div>
+            <div class="guiGe">
+              <h3>网络支持</h3>
+              <dl>
+                <dt>双卡机类型</dt>
+                <dd>双卡双待单通</dd>
+                <dt>最大支持SIM卡数量</dt>
+                <dd>2个</dd>
+                <dt>SIM卡类型</dt>
+                <dd class="Ptable-tips">
+                  <a href="#"><i>？</i></a>
+                </dd>
+                <dd>Nano SIM</dd>
+                <dt>4G网络</dt>
+                <dd class="Ptable-tips">
+                  <a href="#"><i>？</i></a>
+                </dd>
+                <dd>4G：移动（TD-LTE）；4G：联通（FDD-LTE）；4G：电信（FDD-LTE）；4G：联通（TD-LTE）</dd>
+                <dt>3G/2G网络</dt>
+                <dd>3G：移动（TD-LTE）；4G：联通（WCDMA）；3G：电信（CDMA2000）；2G：移动联通（GSM）+ 电信（CDMA）</dd>
+                <dt>副SIM卡类型</dt>
+                <dd class="Ptable-tips">
+                  <a href="#"><i>？</i></a>
+                </dd>
+                <dd>副SIM卡与存储卡二选一</dd>
+                <dt>副SIM卡4G网络</dt>
+                <dd class="Ptable-tips">
+                  <a href="#"><i>？</i></a>
+                </dd>
+                <dd>4G：移动（TD-LTE）；4G：联通（FDD-LTE）；4G：电信（FDD-LTE）；4G：联通（TD-LTE）</dd>
+                <dt>副SIM卡3G/2G网络</dt>
+                <dd>不支持主副卡同时使用电信卡；3G联通（WCDMA）；3G：电信（CDMA2000）；2G：移动联通（GSM）+电信（CDMA）</dd>
+                <dt>网络频率（2G/3G）</dt>
+                <dd>2G：GSM 850/900/1800/1900；2G：CDMA 800；3G：TD-SCDMA 1900/2000；3G：WCDMA 850/900/1900/2100；3G</dd>
+                <dd>CDMA2000；3G：CDMA 800MHz 1X&amp;EVDO ；3G：WCDMA：850/900/1700/1900/2100MHz</dd>
+              </dl>
+            </div>
+            <div class="guiGe">
+              <h3>存储</h3>
+              <dl>
+                <dt>ROM</dt>
+                <dd>128GB</dd>
+                <dt>RAM</dt>
+                <dd>6GB</dd>
+                <dt>存储卡</dt>
+                <dd>支持MicroSD（TF）</dd>
+                <dt>最大存储扩展容量</dt>
+                <dd>256GB</dd>
+              </dl>
+            </div>
+            <div class="package-list">
+              <h3>包装清单</h3>
+              <p>手机（含内置电池） X 1、5A大电流华为SuperCharge充电器X 1、5A USB数据线 X 1、半入耳式线控耳机 X 1、快速指南X 1、三包凭证 X 1、取卡针 X 1、保护壳 X 1</p>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="售后保障">
+          <div class="oBox">
+            <div class="after-sale-title-item">
+              <span class="oImg">
+                <h3>厂家服务</h3>
+              </span>
+              <div class="wenZi">
+                本产品全国联保，享受三包服务，质保期为：一年保<br>
+                如因质量问题或故障，凭厂商维修中心或特约维修点的质量检测证明，享受7日内退货，15日内换货，15日以上在保质期内享受免费保修等安保服务！<br>
+                (注：如厂家在商品介绍中有售后保障的说明，则此商品按照厂家说明执行售后保障服务。)您可以查询本品牌在各地售后服务中心的联系方式<br>
               </div>
-            </li>
-          </ul>
-        </div>
-
-      </div>
+            </div>
+            <div class="after-sale-title-item">
+              <span class="oImg">
+                <h3>平台承诺</h3>
+              </span>
+              <div class="wenZi">
+                本产品全国联保，享受三包服务，质保期为：一年保<br>
+                如因质量问题或故障，凭厂商维修中心或特约维修点的质量检测证明，享受7日内退货，15日内换货，15日以上在保质期内享受免费保修等安保服务！<br>
+                (注：如厂家在商品介绍中有售后保障的说明，则此商品按照厂家说明执行售后保障服务。)您可以查询本品牌在各地售后服务中心的联系方式<br>
+              </div>
+            </div>
+            <div class="after-sale-title-item">
+              <span class="oImg">
+                <h3>正品行货</h3>
+              </span>
+              <div class="wenZi">
+                京东商城向您保证所售商品均为正品行货，京东自营商品开具机打发票或电子发票。<br>
+              </div>
+            </div>
+            <div class="after-sale-title-item">
+              <span class="oImg">
+                <h3>全国联保</h3>
+              </span>
+              <div class="wenZi">
+                凭质保证书及京东商城发票，可享受全国联保服务（奢侈品、钟表除外；奢侈品、钟表由京东联系保修，享受法定三包售后服务），与您亲临商场选购的商<br>
+                品享受相同的质量保证。京东商城还为您提供具有竞争力的商品价格和运费政策，请您放心购买！<br>
+                注：因厂家会在没有任何提前通知的情况下更改产品包装、产地或者一些附件，本司不能确保客户收到的货物与商城图片、产地、附件说明完全一致。只能确保为原厂正货！<br>
+                并且保证与当时市场上同样主流新品一致。若本商城没有及时更新，请大家谅解！<br>
+              </div>
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="商品评价(800+)">// TODO</el-tab-pane>
+      </el-tabs>
       <div class="clearboth"></div>
     </div>
   </div>
@@ -375,5 +529,8 @@ export default {
   activated () {}
 }
 </script>
-<style src="../style/css/product-detail.css" scoped>
+<style src="../style/css/product-detail.css">
+.el-tabs__content {
+  padding: 0 !important;
+}
 </style>
