002d106e3aad297c62fab81e6dad8a5e4dddca9f
hanfeng <hanfeng21050@163.com>
2020-12-05 18:55:31 +0800

    完成登录页面和注册页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="body">
-    <div class="outer6">
-      <h4>登录</h4>
-      <form action="">
-        <div class="div">
-          <input type="text" placeholder="手机/邮箱/用户名" name="username">
+    <div class="regist">
+      <div class="title">
+        <div class="left">
+          登录
         </div>
-        <div class="div">
-          <input type="password" placeholder="登录密码" name="password">
+        <div class="right">
+          没有账号?
+          <router-link to="/regist">立即注册</router-link>
         </div>
-        <div class="inter1">
-          <a href="./青竹注册.html">注册</a>
-          <a href="./忘记密码.html" class="right">忘记密码</a>
-        </div>
-        <input type="submit" value="登录" id="submit">
-      </form>
+      </div>
+      <el-form ref="form" status-icon :rules="rules" :model="form">
+        <el-form-item prop="mobile">
+          <el-input v-model="form.mobile" placeholder="请输入手机号" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-unlock"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="text-align:center">
+          <el-button type="primary" @click="submitForm('form')" :loading="loginLoading">立即登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -23,13 +33,72 @@
 export default {
   components: {},
   data () {
+    var validateMobile = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入手机号码'))
+      } else if (!/^1[3456789]\d{9}$/.test(value)) {
+        callback(new Error('请输入正确的手机号码'))
+      } else {
+        callback()
+      }
+    }
+
+    var validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
     return {
-      data: '12'
+      sendLoading: false,
+      loginLoading: false,
+      countdown: 10,
+      form: {
+        mobile: '',
+        password: '',
+        code: ''
+      },
+      rules: {
+        mobile: [{ validator: validateMobile, trigger: 'blur' }],
+        password: [{ validator: validatePassword, trigger: 'blur' }],
+        code: [{ required: true, message: '请输入验证码', trigger: 'blur' }]
+      }
     }
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    send () {
+      const interval = setInterval(() => {
+        this.countdown--
+        if (this.countdown === 0) {
+          clearInterval(interval)
+          this.sendLoading = false
+          this.countdown = 10
+          return
+        }
+        this.sendLoading = true
+      }, 1000)
+
+      // todo regist
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.loginLoading = true
+          setTimeout(() => {
+            this.loginLoading = false
+            alert('submit!')
+          }, 1000)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    createCode () {}
+  },
   created () {},
   mounted () {},
   beforeCreate () {},
@@ -41,5 +110,37 @@ export default {
   activated () {}
 }
 </script>
-<style scoped src="../style/css/login.css">
+<style scoped>
+.body {
+  background-color: #f2f2f2;
+  padding: 144px 0 80px 0;
+}
+
+.regist {
+  width: 300px;
+  background-color: #ffffff;
+  padding: 50px 50px 20px 50px;
+}
+
+.regist .title {
+  display: flex;
+  padding: 10px 0;
+}
+.regist .title .left {
+  font-size: 20px;
+  font-weight: bolder;
+  flex: 1;
+}
+.regist .title .right {
+  line-height: 30px;
+  font-size: 10px;
+  color: gray;
+}
+.regist .title .right a {
+  color: #e6a23c;
+}
+
+.e-input {
+  width: 300px;
+}
 </style>
--- src/views/regist.vue
+++ src/views/regist.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="body">
-    <div class="outer7">
-      <h4>注册</h4>
-      <form action="">
-        <div class="div">
-          <input type="text" placeholder="手机/邮箱/用户名" name="username">
+    <div class="regist">
+      <div class="title">
+        <div class="left">
+          注册
         </div>
-        <div class="div div1">
-          <input type="text" placeholder="手机验证码" name="code">
-          <span class="sp2">获取验证码</span>
+        <div class="right">
+          已有账号
+          <router-link to="/login">立即登录</router-link>
         </div>
-        <div class="div">
-          <input type="password" placeholder="设置密码" name="password">
-        </div>
-        <div class="div">
-          <input type="password" placeholder="确认密码" name="repassword">
-        </div>
-        <div class="inter1">
-          <span>已有账号，请</span><router-link to="/login">直接登录</router-link>
-        </div>
-        <input type="submit" value="注册" id="submit">
-      </form>
+      </div>
+      <el-form ref="form" status-icon :rules="rules" :model="form">
+        <el-form-item prop="mobile">
+          <el-input v-model="form.mobile" placeholder="请输入手机号" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-unlock"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="checkPassword">
+          <el-input type="password" v-model="form.checkPassword" placeholder="请确认密码" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-unlock"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="form.code" placeholder="请输入验证码" style="width:170px" class="e-input">
+            <i slot="prefix" class="el-input__icon el-icon-edit"></i>
+          </el-input>
+          <el-button type="warning" style="float:right;width:112px;" :disabled="sendLoading" @click="send">{{!sendLoading ? '获取验证码' : countdown+'秒'}}</el-button>
+        </el-form-item>
+        <el-form-item style="text-align:center">
+          <el-button type="primary" @click="submitForm('form')" :loading="registLoading">立即注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -29,11 +49,110 @@
 export default {
   components: {},
   data () {
-    return {}
+    var validateMobile = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入手机号码'))
+      } else if (!/^1[3456789]\d{9}$/.test(value)) {
+        callback(new Error('请输入正确的手机号码'))
+      } else {
+        callback()
+      }
+    }
+
+    var validateUsername = (rule, value, callback) => {
+      if (value === '') {
+        this.userNameCheck = false
+        callback(new Error('请输入用户名'))
+      } else {
+        const flag = true
+        setTimeout(() => {
+          if (flag) {
+            callback()
+          } else {
+            this.userNameCheck = false
+            callback(new Error('用户名已存在'))
+          }
+        }, 1000)
+      }
+    }
+
+    var validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.form.checkPassword !== '') {
+          this.$refs.form.validateField('checkPassword')
+        }
+        callback()
+      }
+    }
+
+    var validatePasswordConfirm = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.form.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+
+    return {
+      sendLoading: false,
+      registLoading: false,
+      countdown: 10,
+      form: {
+        mobile: '',
+        username: '',
+        password: '',
+        checkPassword: '',
+        code: ''
+      },
+      rules: {
+        mobile: [{ validator: validateMobile, trigger: 'blur' }],
+        username: [{ validator: validateUsername, trigger: 'blur' }],
+        password: [{ validator: validatePassword, trigger: 'blur' }],
+        checkPassword: [
+          { validator: validatePasswordConfirm, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' }
+        ]
+      }
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    send () {
+      const interval = setInterval(() => {
+        this.countdown--
+        if (this.countdown === 0) {
+          clearInterval(interval)
+          this.sendLoading = false
+          this.countdown = 10
+          return
+        }
+        this.sendLoading = true
+      }, 1000)
+
+      // todo regist
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.registLoading = true
+          setTimeout(() => {
+            this.registLoading = false
+            alert('submit!')
+          }, 1000)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  },
   created () {},
   mounted () {},
   beforeCreate () {},
@@ -45,5 +164,37 @@ export default {
   activated () {}
 }
 </script>
-<style scoped src="../style/css/regist.css">
+<style scoped>
+.body {
+  background-color: #f2f2f2;
+  padding: 144px 0 80px 0;
+}
+
+.regist {
+  width: 300px;
+  background-color: #ffffff;
+  padding: 50px 50px 20px 50px;
+}
+
+.regist .title {
+  display: flex;
+  padding: 10px 0;
+}
+.regist .title .left {
+  font-size: 20px;
+  font-weight: bolder;
+  flex: 1;
+}
+.regist .title .right {
+  line-height: 30px;
+  font-size: 10px;
+  color: gray;
+}
+.regist .title .right a {
+  color: #e6a23c;
+}
+
+.e-input {
+  width: 300px;
+}
 </style>
